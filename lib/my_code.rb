f8bc5157573ecb9fb225a241b128aca08b0a4ef7
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-18 15:47:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,6 +4,7 @@ def map_to_negativize(source_array)
  while i < source_array.length do
    new.push (source_array[i] * -1)
    i += 1 
- source_array = [1, 2, 3]
-   source_array.reduce[-1] == new []
+   return new
+ #source_array = [1, 2, 3]
+  # source_array.reduce[-1] == new []
   end# My Code here....
